f176bd4a47c6268bc912b221656eb6ac2eed66d6
Yura Chornyi <chorny.yura@gmail.com>
2025-05-02 17:10:47 +0300

    Solution

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,10 +1,59 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
-from sqlalchemy import select
+from fastapi import Request
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_db, MovieModel
-
+from schemas import MovieDetailResponseSchema, MovieListResponseSchema
 
 router = APIRouter()
 
-# Write your code here
+
+@router.get("/movies/{movie_id}/", response_model=MovieDetailResponseSchema)
+async def get_movie(movie_id: int, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(MovieModel).where(MovieModel.id == movie_id))
+    movie = result.scalar_one_or_none()
+    if not movie:
+        raise HTTPException(
+            status_code=404, detail="Movie with the given ID was not found."
+        )
+    return movie
+
+
+@router.get("/movies/", response_model=MovieListResponseSchema)
+async def get_movies(
+    request: Request,
+    db: AsyncSession = Depends(get_db),
+    page: int = Query(1, ge=1),
+    per_page: int = Query(10, ge=1, le=20),
+):
+    offset = (page - 1) * per_page
+    result = await db.execute(select(MovieModel).offset(offset).limit(per_page))
+    movies = result.scalars().all()
+
+    if not movies:
+        raise HTTPException(status_code=404, detail="No movies found.")
+
+    def build_page_url(page_num: int) -> str | None:
+        return (
+            str(request.url_for("get_movies")) + f"?page={page_num}&per_page={per_page}"
+        )
+
+    prev_page = build_page_url(page - 1) if page > 1 else None
+    next_page = build_page_url(page + 1) if len(movies) == per_page else None
+
+    total_items_result = await db.execute(select(func.count()).select_from(MovieModel))
+    total_items = total_items_result.scalar_one_or_none()
+    total_pages = (
+        total_items // per_page
+        if total_items % per_page == 0
+        else total_items // per_page + 1
+    )
+
+    return MovieListResponseSchema(
+        movies=movies,
+        prev_page=prev_page,
+        next_page=next_page,
+        total_pages=total_pages,
+        total_items=total_items,
+    )
--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,36 @@
-# Write your code here
+import datetime
+
+from pydantic import BaseModel
+
+
+class MovieBase(BaseModel):
+    name: str
+    date: datetime.date
+    score: float
+    genre: str
+    overview: str
+    crew: str
+    orig_title: str
+    status: str
+    orig_lang: str
+    budget: float
+    revenue: float
+    country: str
+
+
+class MovieDetailResponseSchema(MovieBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class MovieListResponseSchema(BaseModel):
+    movies: list[MovieDetailResponseSchema]
+    prev_page: str | None
+    next_page: str | None
+    total_pages: int
+    total_items: int
+
+    class Config:
+        from_attributes = True
